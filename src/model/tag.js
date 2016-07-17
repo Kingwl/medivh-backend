ce201fceff38d180dfe74d90464e25db1c6f798b
kingwl <805037171@163.com>
2016-07-17 21:53:32 +0800

    feat: add tag model

---
+++ src/model/tag.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TagSchema = new Schema({
+  name: String,
+  article: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Article',
+    }
+  ]
+});
+
+const Tag = mongoose.model('Tag', TagSchema);
+module.exports = Tag;
