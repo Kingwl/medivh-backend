84253f3b526a9e7ffbbbc98a32e8fe60c5129bf1
碎碎酱 <yinxin630@gmail.com>
2016-07-17 20:17:35 +0800

    feat: initialization project

---
+++ config/config.simple.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // 服务器端口
+    port: 9000,
+
+    // 服务器数据库
+    database: 'mongodb://localhost/database-name',
+
+    // 测试数据库
+    testDatabase: 'mongodb://localhost/database-name-test',
+}
