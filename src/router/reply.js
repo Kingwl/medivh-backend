aa371b97f1a001bc646bb43a19d5c869d6059a9b
kingwl <805037171@163.com>
2016-07-18 23:41:59 +0800

    feat: add reply router

--- src/router/reply.js
+++ src/router/reply.js
@@ -1,23 +1,32 @@
 'use strict'
 
 const router = require('koa-router')();
-const bluebird = require('bluebird');
+const mongoose = require('mongoose');
+const Reply = require('../model/reply');
 
 function register(app) {
     router.get('/reply', function* (next) {
-
+        let replys = yield Reply.find();
+        this.body = replys;
     });
 
     router.get('/reply/:id', function* (next) {
-
+        let {id} = this.params;
+        let reply = yield Reply.findById(id);
+        this.body = reply;
     });
 
     router.put('/reply/:id', function* (next) {
-
+        let {id} = this.params;
+        yield Reply.findByIdAndUpdate(id, this.request.body);
+        let newReply = yield Reply.findById(id);
+        this.body = newReply;
     });
 
     router.delete('/reply/:id', function* (next) {
-
+        let {id} = this.params;
+        yield Reply.findByIdAndRemove(id);
+        this.body = {};
     });
 
     app.use(router.routes());
