576919b3794a0fdd427aa02c462247d8eff11e2e
kingwl <805037171@163.com>
2016-07-19 12:12:51 +0800

    feat: add file router

--- src/router/file.js
+++ src/router/file.js
@@ -1,10 +1,28 @@
 'use strict'
 
 const router = require('koa-router')();
+const Article = require('../model/article');
 
 function register(app) {
     router.get('/file', function* (next) {
+        let articles = yield Article.find();
+        let option = {
+            map: function () {
+                emit(`${this.createTime.getFullYear()}-${this.createTime.getMonth()}`, 1);
+            },
 
+            recude: function (k, values) {
+                return values.length;
+            }
+        };
+
+        let files = yield Article.mapReduce(option);
+        this.body = files;
+    });
+
+    router.get('/file/date/:year/:month', function* (next) {
+        let {year, month} = this.params;
+        
     });
 
     app.use(router.routes());
